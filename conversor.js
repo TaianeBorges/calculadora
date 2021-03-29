7fe2a088920c6b6150c57e47c5945e22371ba6f9
taianeborges <taianealmeidaborges@gmail.com>
2021-03-29 17:20:18 -0300

    first commit

---
+++ conversor.js
@@ -0,0 +1,10 @@
+
+// var valorEmDolarTexto = prompt('Qual valor em dolar voce quer converter?')
+// var varlorEmDolarNum = parseFloat(valorEmDolarTexto)
+
+// var valorEmReal = varlorEmDolarNum * 5.50
+// var valorEmRealFixado = valorEmReal.toFixed(2)
+
+// alert(valorEmRealFixado)
+var nomedojogo = 'Leangue os legends'
+document.write(nomedojogo)
